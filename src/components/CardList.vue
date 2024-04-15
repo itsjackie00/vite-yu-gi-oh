5c87608ed6b18f83b8746b58458ef3293b85388e
itsjackie00 <jackiedanso33@gmail.com>
2024-04-15 18:47:58 +0200

    fix

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-        card list
-        <div v-for="card in store.cards" :key="card.id">
+    <div class="container">
+        <div class="row">
 
-            <!-- <CardComponent :id="card.id" :title="card.name" :image="card.card_images[0].image_url" :status="card.type" :text="card.desc" :subtext="card.atk"/> -->
-            <CardComponent/>
+            <div class="col-12 col-md-6 col-lg-3 " v-for="card in store.card" :key="card.id">
+    
+                <CardComponent :id="card.id" :title="card.name" :image="card.card_images[0].image_url" /> 
+                
+            </div>
         </div>
+        
     </div>
 </template>
 
 <script>
 import {store} from '../store.js';
 import CardComponent from './CardComponent.vue';
+
     export default {
         name: 'CardList',
         components:{
