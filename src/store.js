a4a07b019421a3257df6ce574845e093b94952e3
itsjackie00 <jackiedanso33@gmail.com>
2024-04-15 16:29:37 +0200

    added enpoints

--- src/store.js
+++ src/store.js
@@ -3,6 +3,16 @@ import {reactive} from 'vue';
 export const store = reactive({
     apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
     endPoint: {
+       'name': 'name',
+       'type': 'type',
+       'desc': 'desc',
+       'atk': 'atk',
+       'def': 'def',
+       'level': 'level',
+       'race': 'race',
+       'attribute': 'attribute'
 
-    }
-})
+    },
+    cards: [],
+    
+});
