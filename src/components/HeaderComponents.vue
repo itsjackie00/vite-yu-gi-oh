49a7fa2d4ff4e564dda81da751a2b1250ef827b1
itsjackie00 <jackiedanso33@gmail.com>
2024-04-15 15:24:50 +0200

    created header component

---
+++ src/components/HeaderComponents.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h1>Ciao</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponents',
+        data(){
+            return {
+               
+            }
+        },
+        mounted() {
+           
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
