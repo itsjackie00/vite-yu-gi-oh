d434660bb33d786a99da770f69cf80710eae8159
itsjackie00 <jackiedanso33@gmail.com>
2024-04-15 18:47:52 +0200

    added img

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,17 +1,13 @@
 <template>
     <div class="card">
-        ciao
         <img :src="image" class="card-img-top" :alt="title">
         <div class="card-body">
             <h5 class="card-title">{{ title }}</h5>
             <p class="card-text">
-                <span>Status: {{ status }}</span>
-                <br>
                 <span>{{ text }}</span>
                 <br>
                 <span>{{ subtext }}</span>
             </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
 </template>
@@ -19,7 +15,7 @@
 <script>
     export default {
         name: 'CardComponent',
-        proms: ['id', 'title', 'image', 'status', 'text', 'subtext'], 
+        props: ['id', 'title', 'image', 'status', 'text', 'subtext']
     }
 </script>
 
