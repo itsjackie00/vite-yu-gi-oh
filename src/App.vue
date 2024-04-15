7ca3b0ff8648e7b44534fb34b4fd4397870cfd4b
Jacqueline Wuaku <158096239+itsjackie00@users.noreply.github.com>
2024-04-15 14:51:36 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <h1>Ciao</h1>
+  <i class="fa-solid fa-house"></i>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
