d51e3a8720e2bcab233cfb9162156df9acef7fc9
itsjackie00 <jackiedanso33@gmail.com>
2024-04-15 16:36:40 +0200

    imported cardlist

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <CardList/>
+    </main>
+</template>
+
+<script>
+import CardList from './CardList.vue';
+
+    export default {
+        name: 'MainComponent',
+        components:{
+            CardList
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
