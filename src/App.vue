28743b9fb2a1403bb8cbf15ed35e5eedb4585fd9
itsjackie00 <jackiedanso33@gmail.com>
2024-04-15 15:24:28 +0200

    import store from store.js

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 
 <template>
-  <h1>Ciao</h1>
-  <i class="fa-solid fa-house"></i>
+  <HeaderComponents/>
 </template>
 
 <script>
+import {store} from './store.js';
+
+  import HeaderComponents from './components/HeaderComponents.vue';
+
   export default {
     name: 'App',
+    components: {
+      HeaderComponents
+    },
+    data() {
+      return {
+        store
+      }
+    },
+    mounted() {
+      console.log(this.store);
+    }
   }
 </script>
 
