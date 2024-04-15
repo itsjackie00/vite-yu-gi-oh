2e187c22953e919d5efbe521dce09e45d71dea60
itsjackie00 <jackiedanso33@gmail.com>
2024-04-15 16:37:09 +0200

    imported store

---
+++ src/components/CardList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        card list
+        <div v-for="card in store.cards" :key="card.id">
+
+            <!-- <CardComponent :id="card.id" :title="card.name" :image="card.card_images[0].image_url" :status="card.type" :text="card.desc" :subtext="card.atk"/> -->
+            <CardComponent/>
+        </div>
+    </div>
+</template>
+
+<script>
+import {store} from '../store.js';
+import CardComponent from './CardComponent.vue';
+    export default {
+        name: 'CardList',
+        components:{
+            CardComponent
+        },
+        data() {
+            return {
+                store
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
