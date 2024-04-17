f98fdb06235284dd3d284a7838a6a341df1185af
itsjackie00 <jackiedanso33@gmail.com>
2024-04-17 09:44:37 +0200

    added get archetype function

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,67 @@
-
 <template>
-  <HeaderComponents/>
-  <MainComponent/>
+  <HeaderComponents @statusSearch="setParams" />
+  <MainComponent />
 </template>
 
 
 <script>
-import {store} from './store.js';
+import { store } from './store.js';
 import axios from 'axios';
 
 import HeaderComponents from './components/HeaderComponents.vue'
 import MainComponent from './components/MainComponent.vue'
 
-  export default {
-    name: 'App',
-    components: {
-      HeaderComponents,
-      MainComponent
-    },
-    data() {
-      return {
-        store
-      }
-    },
-    methods: {
-      getCharacters(){
-        axios.get(this.store.apiUrl ).then((res) => {
-          this.store.card = res.data.data,
-          console.log(this.store)
-          
-        })
-      },
-    },
-    created(){
-    this.getCharacters();
+export default {
+  name: 'App',
+  components: {
+    HeaderComponents,
+    MainComponent
   },
-    mounted() {
-      //console.log(this.store)
-     
+  data() {
+    return {
+      store
     }
+  },
+  methods: {
+    setParams() {
+      const options = {};
+      if (this.store.statusFilter) {
+        options.params = {
+          status: this.store.statusFilter
+        }
+      }
+      this.getCharacters(options);
+    },
+    getCharacters() {
+      axios.get(this.store.apiUrl + this.store.endPoint.cards).then((res) => {
+        //console.log(res.data);
+        this.store.cards = res.data.data.map((card) => {
+          return {
+            id: card.id,
+            title: card.name,
+            image: card.card_images[0].image_url,
+            status: card.archetype
+          }
+        })
+        console.log(this.store.cards);
+      })
+    },
+    getArchetype() {
+      axios.get(this.store.apiUrl + this.store.endPoint.archetype).then((res) => {
+         this.store.archetypeList = res.data.data.slice(0, 10);
+        console.log(this.store.archetypeList);
+      })
+    }
+  },
+  created() {
+    this.getCharacters();
+    this.getArchetype();
+  },
+  mounted() {
+    //console.log(this.store)
+
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
