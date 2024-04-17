a75e2e867533198994055abd874924793d72a480
itsjackie00 <jackiedanso33@gmail.com>
2024-04-17 09:44:45 +0200

    added option and params

--- src/store.js
+++ src/store.js
@@ -3,16 +3,18 @@ import {reactive} from 'vue';
 export const store = reactive({
     apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
     endPoint: {
-       'name': 'name',
-       'type': 'type',
-       'desc': 'desc',
-       'atk': 'atk',
-       'def': 'def',
-       'level': 'level',
-       'race': 'race',
-       'attribute': 'attribute'
-
+       cards: 'cardinfo.php',
+       archetype: 'archetypes.php'
+    },
+    options: {
+        params: {
+            num: 20,
+            offset: 0,
+            status:''
+        }
     },
     cards: [],
+    statusFilter: '',
+    archetypeList: []
     
 });
